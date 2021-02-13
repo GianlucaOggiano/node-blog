3b2e6c28a366de13b59ebf56167be7a31a26f18f
Gianluca Oggiano <gianlucao89@gmail.com>
2021-02-13 10:29:06 +0100

    Minor changes

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const exphbs = require('express-handlebars');
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
+const chalk = require('chalk');
 const dotenv = require('dotenv');
 
 const authRoutes = require('./routes/auth');
+const { feather } = require('./helpers/hbs');
 
 dotenv.config();
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+console.log('NODE_ENV', isDevelopment);
 
 (async () => {
   try {
@@ -16,14 +24,21 @@ dotenv.config();
       useCreateIndex: true,
       useUnifiedTopology: true,
     });
-    mongoose.set('debug', true);
+    isDevelopment ? mongoose.set('debug', true) : null;
 
     const app = express();
 
-    app.engine('.hbs', exphbs({ extname: '.hbs' }));
+    app.use(helmet());
+    app.use(compression());
+    isDevelopment ? app.use(morgan('dev')) : null;
+
+    app.engine('.hbs', exphbs({ extname: '.hbs', helpers: { feather } }));
     app.set('view engine', '.hbs');
 
     app.use(express.static(path.join(__dirname, 'public')));
+    app.use(
+      express.static(path.join(__dirname, 'node_modules', 'feather-icons'))
+    );
 
     app.get('/', (req, res) => {
       res.render('home', { title: 'Blog | Home', description: 'Lorem Ipsum' });
@@ -31,15 +46,33 @@ dotenv.config();
 
     app.use(authRoutes);
 
-    app.use((req, res) => {
-      res.status(404).render('errors/404', { title: 'Page not found' });
+    /* Not found error */
+    app.use((req, res, next) => {
+      const error = new Error('Page not found.');
+      error.status = 404;
+      next(error);
+    });
+
+    /* Global error handler */
+    app.use((error, req, res, next) => {
+      const statusCode = error.status === 200 ? 500 : error.status;
+      res.status(statusCode).render('error', {
+        title: error.message,
+        message: error.message,
+        error: isDevelopment ? error.stack : '',
+      });
     });
 
     const PORT = process.env.PORT || 4000;
     app.listen(PORT, () => {
-      console.log(`Server started on port ${PORT}`);
+      console.clear();
+      console.log(
+        chalk.cyan.bold(
+          `Server started in ${process.env.NODE_ENV} mode on port ${PORT}`
+        )
+      );
     });
   } catch (error) {
-    console.log(error);
+    console.log(chalk.red(error));
   }
 })();
