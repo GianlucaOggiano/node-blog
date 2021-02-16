58bb3fdc785a55316b236c2e6731af7b2566fc2c
Gianluca Oggiano <gianlucao89@gmail.com>
2021-02-16 17:49:12 +0100

    Get all articles

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,13 @@ const router = require('express').Router();
 
 const Article = require('../models/Article');
 
-router.get('/', async (req, res) => {
+router.get('/', async (req, res, next) => {
   try {
-    const articles = await Article.find({});
-    console.log({ articles });
+    const articles = await Article.find({}).populate('author').lean();
     res.render('home', {
       title: 'Home',
       description: 'Lorem Ipsum',
+      articles: articles,
     });
   } catch (error) {
     next(error);
